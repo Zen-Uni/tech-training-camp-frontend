df48ecb4990e89c81a412d04f4e51e6a5a4738b5
Uni <zunicoco@163.com>
2021-03-17 00:11:49 +0800

    feat: 后端更新文章模型

--- server/db/index.js
+++ server/db/index.js
@@ -22,13 +22,34 @@
      },
      captcha: String
  })
+
+// TODO:设定文章模型
+ let cut = 0
+ const articleSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        default: () => {
+            return cut++
+        }
+    },
+    title: String,
+    content: String,
+    username: String,
+    share: {
+        type: Boolean,
+        default: false
+    },
+    email: String
+ })
  
  
  //define data model
  const User = mongoose.model('users', userSchema)
  const Captcha = mongoose.model('captchas', captchaSchema)
+ const Article = mongoose.model('articles', articleSchema)
  
  module.exports = {
      User,
      Captcha,
+     Article,
  }
